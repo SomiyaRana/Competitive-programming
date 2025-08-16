dfd2d46233fe37ee771687091a9bb9ddaf095cc3
Somiya Rana <157635265+SomiyaRana@users.noreply.github.com>
2025-08-16 20:10:37 +0530

    Time: 5 ms (79.13%), Space: 42.1 MB (80.18%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        int[] lastIndex = new int[26];
+
+        // Step 1: Store the last occurrence index of each character
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        // Step 2: Traverse the string and determine partitions
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+
+            if (i == end) {  // A partition is found
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+}
