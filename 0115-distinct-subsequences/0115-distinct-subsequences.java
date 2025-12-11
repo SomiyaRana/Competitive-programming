cb1dbfb4b053fe5c74316a1ab19433a5c5367ccc
Somi.yayaya <somyarana819@gmail.com>
2025-12-11 15:01:00 +0530

    Time: 21 ms (32.47%), Space: 54.3 MB (45.18%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -7,9 +7,11 @@ class Solution {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j];     
+
+                dp[i][j] = dp[i - 1][j];
+
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] += dp[i - 1][j - 1];   
+                    dp[i][j] += dp[i - 1][j - 1];
                 }
             }
         }
