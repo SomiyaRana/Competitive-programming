c58257c6038c7fc04bffa26b9af03e664eea25cf
Somi.yayaya <somyarana819@gmail.com>
2025-12-26 19:06:43 +0530

    Time: 32 ms (32.99%), Space: 48.8 MB (11.92%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> types = new HashSet<>();
+        
+        for (int c : candyType) {
+            types.add(c);
+        }
+        
+        return Math.min(types.size(), candyType.length / 2);
+    }
+}
