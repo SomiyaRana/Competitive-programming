38077b5513e56cdf59f5117fc879abb324750857
Somi.yayaya <somyarana819@gmail.com>
2026-01-07 14:54:44 +0530

    Time: 8 ms (32.68%), Space: 59.3 MB (78.99%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -0,0 +1,32 @@
+class Solution {
+    long totalSum = 0;
+    long maxProduct = 0;
+    final int MOD = 1_000_000_007;
+
+    public int maxProduct(TreeNode root) {
+        totalSum = treeSum(root);
+
+        computeProduct(root);
+
+        return (int)(maxProduct % MOD);
+    }
+
+    private long treeSum(TreeNode node) {
+        if (node == null) return 0;
+        return node.val + treeSum(node.left) + treeSum(node.right);
+    }
+
+    private long computeProduct(TreeNode node) {
+        if (node == null) return 0;
+
+        long left = computeProduct(node.left);
+        long right = computeProduct(node.right);
+
+        long subTreeSum = node.val + left + right;
+
+        long product = subTreeSum * (totalSum - subTreeSum);
+        maxProduct = Math.max(maxProduct, product);
+
+        return subTreeSum;
+    }
+}
