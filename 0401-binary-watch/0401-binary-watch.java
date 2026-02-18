f45c13b32984e76454f3a7ac41e40b6545f061b3
Somi.yayaya <somyarana819@gmail.com>
2026-02-18 15:52:12 +0530

    Time: 15 ms (8.25%), Space: 49.9 MB (5.53%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+        
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+                
+                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+                
+                if (totalBits == turnedOn) {
+                    result.add(hour + ":" + String.format("%02d", minute));
+                }
+            }
+        }
+        
+        return result;
+    }
+}
