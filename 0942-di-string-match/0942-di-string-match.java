86cfa22274204df6e5254fd33c31809d7675f1fa
Somi.yayaya <somyarana819@gmail.com>
2026-03-20 11:56:55 +0530

    Time: 2 ms (99.53%), Space: 47.4 MB (38.64%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int low = 0, high = n;
+        
+        int[] result = new int[n + 1];
+        
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                result[i] = low++;
+            } else {
+                result[i] = high--;
+            }
+        }
+        
+        // Last remaining number
+        result[n] = low; 
+        
+        return result;
+    }
+}
