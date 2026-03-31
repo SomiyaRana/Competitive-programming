13551615f95763e3f3a3db7963bbc95a6eb681a3
Somi.yayaya <somyarana819@gmail.com>
2026-03-31 09:55:07 +0530

    Time: 1 ms (100%), Space: 45.7 MB (98.55%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int maxTime = releaseTimes[0];
+        char result = keysPressed.charAt(0);
+
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int duration = releaseTimes[i] - releaseTimes[i - 1];
+            char ch = keysPressed.charAt(i);
+
+            if (duration > maxTime || 
+               (duration == maxTime && ch > result)) {
+                maxTime = duration;
+                result = ch;
+            }
+        }
+
+        return result;
+    }
+}
