b282df9aee865265197978dd681ac2a82674f74a
Somi.yayaya <somyarana819@gmail.com>
2026-04-01 11:07:23 +0530

    Time: 0 ms (100%), Space: 42.4 MB (98.7%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Case 1: both null
+        if (p == null && q == null) return true;
+
+        // Case 2: one null or values differ
+        if (p == null || q == null || p.val != q.val) return false;
+
+        // Case 3: check left and right
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
