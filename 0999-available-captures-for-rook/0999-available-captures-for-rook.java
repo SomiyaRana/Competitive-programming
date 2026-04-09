5ab90cb2f289185af0fccf65a2e243eeea26d26b
Somi.yayaya <somyarana819@gmail.com>
2026-04-09 10:52:52 +0530

    Time: 0 ms (100%), Space: 42.7 MB (73.57%) - LeetHub

---
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numRookCaptures(char[][] board) {
+        int r = 0, c = 0;
+
+        // Find rook position
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == 'R') {
+                    r = i;
+                    c = j;
+                }
+            }
+        }
+
+        int count = 0;
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        // Explore all 4 directions
+        for (int[] d : dirs) {
+            int x = r + d[0];
+            int y = c + d[1];
+
+            while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+                if (board[x][y] == 'B') break; // blocked
+                if (board[x][y] == 'p') {
+                    count++;
+                    break;
+                }
+                x += d[0];
+                y += d[1];
+            }
+        }
+
+        return count;
+    }
+}
