ab133154d501cbef1c4efb067a4dd126fcc0a050
Somi.yayaya <somyarana819@gmail.com>
2026-04-12 16:53:15 +0530

    Time: 17 ms (68.77%), Space: 47.6 MB (65.01%) - LeetHub

---
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        String result = "";
+        
+        for (String word : dictionary) {
+            if (isSubsequence(word, s)) {
+                
+                // choose better answer
+                if (word.length() > result.length() || 
+                   (word.length() == result.length() && word.compareTo(result) < 0)) {
+                    result = word;
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    private boolean isSubsequence(String word, String s) {
+        int i = 0, j = 0;
+        while (i < word.length() && j < s.length()) {
+            if (word.charAt(i) == s.charAt(j)) i++;
+            j++;
+        }
+        return i == word.length();
+    }
+}
