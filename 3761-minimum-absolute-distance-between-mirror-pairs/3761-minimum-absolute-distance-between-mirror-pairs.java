15a86bae9b38a779a916f2db3c1da9fd89447b25
Somi.yayaya <somyarana819@gmail.com>
2026-04-19 14:33:53 +0530

    Time: 51 ms (52.29%), Space: 95.4 MB (28.51%) - LeetHub

---
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int minMirrorPairDistance(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int minDist = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                minDist = Math.min(minDist, i - map.get(nums[i]));
+            }
+
+            int rev = reverse(nums[i]);
+            map.put(rev, i); // ✅ store reversed value
+        }
+
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+
+    private int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+        return rev;
+    }
+}
