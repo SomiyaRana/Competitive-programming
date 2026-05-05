f9526a3888be7358b523d6f9458f6dc746a011fa
Somi.yayaya <somyarana819@gmail.com>
2026-05-05 17:37:45 +0530

    Time: 0 ms (100%), Space: 44.3 MB (44.03%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode curr = head;
+        int n = 1;
+        while (curr.next != null) {
+            curr = curr.next;
+            n++;
+        }
+
+        curr.next = head;
+
+        k = k % n;
+        int stepsToNewTail = n - k;
+
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
