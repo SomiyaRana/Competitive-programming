4819209ef9faa8c64411f5a816a56104863395de
Somi.yayaya <somyarana819@gmail.com>
2026-05-19 20:59:58 +0530

    Time: 0 ms (100%), Space: 45.6 MB (40.18%) - LeetHub

---
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+
+        // Find max of each row and column
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
+            }
+        }
+
+        int totalIncrease = 0;
+
+        // Calculate maximum possible increase
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int allowedHeight = Math.min(rowMax[i], colMax[j]);
+
+                totalIncrease += allowedHeight - grid[i][j];
+            }
+        }
+
+        return totalIncrease;
+    }
+}
