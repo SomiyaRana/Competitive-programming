3bfc3b10fb0578917c056595dfab6ab90e33ea7a
Somi.yayaya <somyarana819@gmail.com>
2026-05-21 09:26:20 +0530

    Time: 27 ms (79.43%), Space: 48.4 MB (39.32%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isPossible(int[] nums) {
+        
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        HashMap<Integer, Integer> need = new HashMap<>();
+
+        // Count frequency
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums) {
+
+            // Already used
+            if (freq.get(num) == 0) {
+                continue;
+            }
+
+            // Use current number
+            freq.put(num, freq.get(num) - 1);
+
+            // Extend existing subsequence
+            if (need.getOrDefault(num, 0) > 0) {
+
+                need.put(num, need.get(num) - 1);
+                need.put(num + 1, need.getOrDefault(num + 1, 0) + 1);
+            }
+
+            // Create new subsequence
+            else if (freq.getOrDefault(num + 1, 0) > 0 &&
+                     freq.getOrDefault(num + 2, 0) > 0) {
+
+                freq.put(num + 1, freq.get(num + 1) - 1);
+                freq.put(num + 2, freq.get(num + 2) - 1);
+
+                need.put(num + 3, need.getOrDefault(num + 3, 0) + 1);
+            }
+
+            // Not possible
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
